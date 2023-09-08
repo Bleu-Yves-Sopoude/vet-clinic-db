9024ddc2e59d7ffc8e6ec0b23871c16a1fb3ae2e
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-09-08 15:14:20 +0000

    Query-update

--- queries.sql
+++ queries.sql
@@ -16,3 +16,66 @@ SELECT * FROM animals WHERE neutered=true;
 SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.13 ;
+
+
+-- Query and Update animals table
+
+BEGIN;
+
+UPDATE animals SET species='unspecified';
+
+ROLLBACK;
+
+
+BEGIN;
+
+UPDATE animals
+SET species ='digimon'
+WHERE name like '%mon';
+
+
+UPDATE animals
+SET species ='pokemon'
+WHERE name NOT like '%mon';
+
+COMMIT;
+
+BEGIN;
+
+DELETE FROM animals;
+
+ROLLBACK;
+
+
+BEGIN;
+
+SAVEPOINT SP1;
+
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT SP2;
+
+UPDATE animals SET weight_kg=weight_kg * (-1);
+
+ROLLBACK TO SP1;
+
+UPDATE animals SET weight_kg=weight_kg * (-1) WHERE weight_kg < -1;
+
+COMMIT;
+
+
+
+
+SELECT COUNT(id) FROM animals;
+
+SELECT COUNT(id) FROM animals WHERE escape_attempts=0;
+
+SELECT AVG(weight_kg) as Average FROM animals;
+
+SELECT neutered, AVG(escape_attempts) FROM animals GROUP BY neutered;
+
+SELECT species , MIN(weight_kg),MAX(weight_kg) FROM animals GROUP BY species;
+
+SELECT species, AVG(escape_attempts) FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
