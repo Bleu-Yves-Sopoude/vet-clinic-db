c4869f564aec28afa19ed6ab01ec1e7c4905bd0e
Bleu-Yves-Sopoude <bleuyves@gmail.com>
2023-09-08 15:50:15 +0000

    Fix

--- data.sql
+++ data.sql
@@ -12,10 +12,10 @@ VALUES('Agumon', '2020-02-03', 0, true,10.23),
 
 
 INSERT INTO animals(name,date_of_birth, escape_attempts,neutered,weight_kg,species) 
-VALUES('Charmander', '2020-02-08', 0, true,-11,NULL),
+VALUES('Charmander', '2020-02-08', 0, false,-11,NULL),
      ('Plantmon', '2021-11-15', 2, true,-5.7,NULL),
      ('Squirtle', '1993-04-02', 3, false,-12.13,NULL),
-     ('Agemon', '2005-06-12', 1, true,-45,NULL),
+     ('Angemon', '2005-06-12', 1, true,-45,NULL),
      ('Boarmon', '2005-06-07', 7, true,20.4,NULL),
      ('Blossom', '1998-10-13', 3, true,17,NULL),
      ('Ditto', '2022-05-14', 4, true,22,NULL);
--- queries.sql
+++ queries.sql
@@ -59,7 +59,7 @@ UPDATE animals SET weight_kg=weight_kg * (-1);
 
 ROLLBACK TO SP1;
 
-UPDATE animals SET weight_kg=weight_kg * (-1) WHERE weight_kg < -1;
+UPDATE animals SET weight_kg * (-1) WHERE weight_kg < 0;
 
 COMMIT;
 
